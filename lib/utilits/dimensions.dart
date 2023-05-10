5ded540268bc0f468835739d3105bdf4e660e0ee
Emanuel Disthambo Djuma <emanueltchambo@gmail.com>
2023-05-10 09:44:23 +0100

    Alteração e implemetação de Lista de imagens

--- lib/utilits/dimensions.dart
+++ lib/utilits/dimensions.dart
@@ -1,5 +1,4 @@
 import 'package:get/utils.dart';
-
 import 'package:get/get.dart';
 
 //Classe de dimensões classificação
@@ -10,4 +9,6 @@ class Dimensions {
   static double pageView = screenHeigth / 2.64;
   static double pageViewContainer = screenHeigth / 3.84;
   static double pageTextContainer = screenHeigth / 7.03;
+  static double heigth10 = screenHeigth / 84.4;
+  static double heigth20 = screenHeigth / 42.2;
 }
